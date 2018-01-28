b966822cf38e9d71022bfa6e73860a135391bc6c
Meikaijie <christophermei@Christophers-MacBook-Pro.local>
2018-01-28 16:58:16 -0500

    debug

--- Serverside.py
+++ Serverside.py
@@ -20,9 +20,6 @@ def requestHandler():
 	print("Received")
 	print(request.form)
 
-def main():
-	pass
-
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
