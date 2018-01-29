149fbf50a7d9046787914d695859fddf2bc09de0
Meikaijie <christophermei@Christophers-MacBook-Pro.local>
2018-01-29 11:15:48 -0500

    some clean up

--- Clientside.py
+++ Clientside.py
@@ -10,26 +10,32 @@ def main():
 
 def requestREPL(url):
 	while True:
-		compound = None
-		compound_logic = None
-		compound_ind = None
-		raw_request = raw_input('please enter a request of the form - \ncommand, compound:formula:logic, property1:value1:logic1, property2:value2:logic2, etc.\n')
+		# compound = None
+		# compound_logic = None
+		# compound_ind = None
+		raw_request = raw_input('please enter a request of the form - \ncommand, compound:logic, property1:value1:logic1, property2:value2:logic2, etc.\n')
 		request_list = raw_request.split(',')
-		if len(request_list) == 0:
+		if len(request_list) < 2:
+			print("A request requires at least a command, and a compound")
 			continue
 		# Strip extra spaces
 		for i in range(len(request_list)):
 			request_list[i] = " ".join(request_list[i].split())
-			if "compound" in request_list[i].lower():
-				compound = request_list[i].split(":")[1].strip()
-				compound_logic = request_list[i].split(":")[2].strip()
-				compound_ind = i
+			# if "compound" in request_list[i].lower():
+			# 	compound = request_list[i].split(":")[1].strip()
+			# 	compound_logic = request_list[i].split(":")[2].strip()
+			# 	compound_ind = i
 		command = request_list[0]
+		compound = request_list[1].split(":")[0].strip()
+		compound_logic = request_list[1].split(":")][1].strip()
+		property_list = []
+		if len(request_list)>2:
+			property_list = request_list[2:]
 
 		if command == "quit":
 			break
-		if not compound_ind is None:
-			property_list = request_list[1:compound_ind] + request_list[compound_ind+1:]
+		# if not compound_ind is None:
+		# 	property_list = request_list[1:compound_ind] + request_list[compound_ind+1:]
 
 		if command.lower() == "add":
 			if compound is None:
@@ -47,7 +53,7 @@ def requestREPL(url):
 		### Extra commands go here
 		elif command.lower() == "create table":
 			tablename = raw_input('please enter a table name\n')
-			columns = raw_input('please enter column name:column type pairs separated by commas\n').split(",")
+			columns = raw_input('please enter column names and column types in the format -\n name1:type1, name2:type2, etc.\n').split(",")
 			response = requests.post(url+"/data/create",json=buildCreateDict(tablename,columns))
 			print(response.text)
 		###
